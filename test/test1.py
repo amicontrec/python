2e5257d9c6156286890963872b37af516e9a974f
Heidy A. <amicontrec@gmail.com>
2026-08-26 18:40:11 -0500

    algoritmo que verifica si un número es impar o par

--- test/test1.py
+++ test/test1.py
@@ -1,1 +1,7 @@
-print("Hola mundo")
+# Determinar si el número ingresado es par o impar
+n = int(input("Ingresar número: "))
+
+if(n % 2 == 0):
+    print("Es un número par")
+else:
+    print("Es un número impar")
